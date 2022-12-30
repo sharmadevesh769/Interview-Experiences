c4377083cc4b7a06f4647001ca89c8fa9b65630e
Devesh Kumar Sharma <sharmadevesh769@gmail.com>
2022-12-30 17:49:33 +0530

    Exp

--- intexp/src/Components/Experience.js
+++ intexp/src/Components/Experience.js
@@ -3,6 +3,8 @@ import jwt from 'jwt-decode'
 import { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { Card, Icon } from 'semantic-ui-react'
+
 
 const Experience = () => {
     const nav = useNavigate()
@@ -15,7 +17,7 @@ const Experience = () => {
     const token = localStorage.getItem('TOKEN');
     async function getData() {
         try {
-            const { data: { exp } } = await await axios.get('http://localhost:8000/api/exp', { headers: { 'Authorization': 'Bearer ' + token } })
+            const { data: { exp } } = await axios.get('http://localhost:8000/api/exp', { headers: { 'Authorization': 'Bearer ' + token } })
             setExperience(exp)
             // console.log(data)
             console.log(experience)
@@ -38,6 +40,9 @@ const Experience = () => {
         }
     }
 
+    async function delExp(){
+        
+    }
     const handlechange = (e) => {
         const { name, value } = e.target
         setaddExp({
@@ -46,39 +51,40 @@ const Experience = () => {
         })
     }
     const addExp = async (e) => {
-        e.preventDefault()
-        // console.log("HELLO")   
         const ex = await axios.post("http://localhost:8000/api/exp", addexp, { headers: { 'Authorization': 'Bearer ' + token } })
-
+        e.preventDefault()
     }
     useEffect(() => {
 
         getExp();
         getData()
-    }, [])
+    }, [experience])
     return (
         <div className='Experience'>
             <div className='AddExp'>
                 <input type="text" name='company'
                     onChange={(e) => handlechange(e)} placeholder='Company Name' />
                 <input type="text" name='batch'
-                    onChange={(e) => handlechange(e)} placeholder='BATCH' />
-                <input type="text" name='desc'
+                    onChange={(e) => handlechange(e)} placeholder='Grad Year' />
+                <textarea rows="10" cols="50" type="textArea" name='desc'
                     onChange={(e) => handlechange(e)} placeholder='Interview Experience' />
                 <button type="submit" onClick={addExp} >Add Exp</button>
             </div>
-
-            {experience.map(exp =>
-                <div className='ShowExp'>
-                    <h2>{exp.company}</h2>
-                    <div className='NB'>
-                        <h3>{exp.batch}</h3>
-                        <h3>Name</h3>
+            <div className='MasterExp'>
+                {experience.map(exp =>
+                    <div className='ShowExp'>
+                        <h2>{exp.company}</h2>
+                        <div className='NB'>
+                            <h3>{exp.batch}</h3>
+                            <h3>Name</h3>
+                        </div>
+                        <p className='descP'>{exp.desc}</p>
+                        <button type="submit">DELETE</button>
                     </div>
-                    <p>{exp.desc}</p>
-                </div>
 
-            )}
+                )}
+            </div>
+
 
         </div>
     )
